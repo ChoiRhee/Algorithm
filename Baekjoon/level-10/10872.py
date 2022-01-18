f31eeb3e38aa3200676e0b25218955efa4cf424e
ChoiRhee <choiree@gmail.com>
2022-01-19 03:22:23 +0900

    2022.01.19

---
+++ Baekjoon/level-10/10872.py
@@ -0,0 +1,18 @@
+#n = int(input())
+#ans = 1
+#if n == 0:
+#    ans = ans
+#else:
+#    for i in range(n):
+#        ans *= (i+1)
+#
+#print(ans)
+
+n = int(input())
+ans = 1
+
+if n >0:
+    for i in range(n):
+        ans *= (i+1)
+
+print(ans)
